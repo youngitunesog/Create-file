453eb0808c361affc9232bb63228c4f353bb0d05
youngitunesog <82830391+youngitunesog@users.noreply.github.com>
2023-08-16 10:50:43 +0100

    Update handle.php

--- handle.php
+++ handle.php
@@ -1,7 +1,7 @@
 <?php
 $txt = _POST["txt"];
 $name = _POST["file-name"];
-$myfile = fopen($name, "w") or die("Unable to open file!");
+$myfile = fopen("hi.html", "w") or die("Unable to open file!");
 echo "done" .fwrite($myfile, $txt);
 fclose($myfile);
 
